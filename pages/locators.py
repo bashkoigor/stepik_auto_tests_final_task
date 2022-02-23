574347be9954d3ce4e7b94bef9493df83a4940a3
Igor Bashko <igor.bashko@yandex.ru>
2022-02-23 22:38:11 +0200

    Init LoginPage

---
+++ pages/locators.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginPageLocators():
+    LOGIN_LINK = (By.ID, "login_link")
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")

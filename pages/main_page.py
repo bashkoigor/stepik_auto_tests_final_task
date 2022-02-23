a2a4b75309eff7fec5ec14db423042640b5c8f3a
Igor Bashko <igor.bashko@yandex.ru>
2022-02-23 23:22:24 +0200

    Page transitions

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .login_page import LoginPage
 from .locators import LoginPageLocators
 
 
@@ -6,4 +7,4 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*LoginPageLocators.LOGIN_LINK)
         login_link.click()
-
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)

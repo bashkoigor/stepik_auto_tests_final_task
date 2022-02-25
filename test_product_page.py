aed0d6918c63fb519bdce7144d78909243aeb746
Igor Bashko <igor.bashko@yandex.ru>
2022-02-25 23:37:03 +0200

    inheritance and negative checks

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,18 @@
 import pytest
 
+from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
+
+
+@pytest.mark.new
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_products_in_basket()
+    basket_page.should_be_text_basket_is_empty()
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
@@ -16,50 +28,54 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
-    @pytest.mark.xfail
-    def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-        page = ProductPage(browser, link)
-        page.open()
-        page.add_to_cart()
-        page.should_not_be_success_message()
 
-    @pytest.mark.xfail
-    def test_guest_cant_see_success_message(browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-        page = ProductPage(browser, link)
-        page.open()
-        page.should_not_be_success_message()
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.should_not_be_success_message()
 
-    @pytest.mark.xfail
-    def test_message_disappeared_after_adding_product_to_basket(browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-        page = ProductPage(browser, link)
-        page.open()
-        page.add_to_cart()
-        page.success_message_should_be_disappeared()
 
-    xfail_link = pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                              marks=pytest.mark.xfail)
+@pytest.mark.xfail
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
 
-    @pytest.mark.parametrize('link',
-                             ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                              xfail_link,
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-    def test_guest_can_add_product_to_basket(browser, link):
-        page = ProductPage(browser, link)
-        page.open()
-        page.add_to_cart()
-        page.solve_quiz_and_get_code()
-        page.did_product_add_to_cart()
-        page.is_price_match()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.success_message_should_be_disappeared()
+
+
+xfail_link = pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                          marks=pytest.mark.xfail)
+
+
+@pytest.mark.parametrize('link',
+                         ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                          xfail_link,
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.solve_quiz_and_get_code()
+    page.did_product_add_to_cart()
+    page.is_price_match()
 
 
 

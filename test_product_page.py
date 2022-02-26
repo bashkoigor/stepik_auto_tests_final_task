eaf55fcd4b602627eef6d0c82b35420fcabeae65
Igor Bashko <igor.bashko@yandex.ru>
2022-02-26 16:52:47 +0200

    minor fixes

--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,6 @@ from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
 
 
-@pytest.mark.new
 class TestUserAddToBasketFromProductPage():
 
     @pytest.fixture(scope="function", autouse=True)
@@ -20,12 +19,14 @@ class TestUserAddToBasketFromProductPage():
         page.register_new_user(email, password)
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear"
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear"
         page = ProductPage(browser, link)
@@ -45,6 +46,7 @@ def test_message_disappeared_after_adding_product_to_basket(self, browser):
     page.success_message_should_be_disappeared()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
     page = ProductPage(browser, link)
@@ -62,12 +64,15 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)

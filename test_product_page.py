3416f1bee8c34cbe658b9aca26fae97b1b4d2971
Igor Bashko <igor.bashko@yandex.ru>
2022-02-26 17:34:26 +0200

    minor fix

--- test_product_page.py
+++ test_product_page.py
@@ -104,6 +104,7 @@ xfail_link = pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coder
                           xfail_link,
                           "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                           "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
